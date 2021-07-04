d3ccc6e6ae8af53a1b8dea7c218237aacf90d0c3
cxs-coding <57263388+cxs-coding@users.noreply.github.com>
2021-07-04 21:30:52 +0800

    Create 104.c

---
+++ 104.c
@@ -0,0 +1,14 @@
+int maxval(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+int maxDepth(struct TreeNode *root)
+{
+    if (root == NULL)
+        return 0;
+    else
+        return 1 + maxval(maxDepth(root->left), maxDepth(root->right));
+}
